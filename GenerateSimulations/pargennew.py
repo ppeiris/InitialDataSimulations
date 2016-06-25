a29c65b674b57a445cca79c48e2f02e027ca3ae1
Prabath Peiris <prabath.peiris@ftr.com>
2016-06-25 03:17:52 -0400

    update aprgen

--- GenerateSimulations/pargennew.py
+++ GenerateSimulations/pargennew.py
@@ -30,100 +30,14 @@ if (options.file is None) or (not os.path.exists(options.file)):
 
 datafile = os.path.join(os.getcwd(), options.file)
 
-inidf = pd.DataFrame()
-
-
-
-# Near Region Parameters
-xmin_near_shift = 0.0625
-xmax_near_shift = 0.0625
-ymin_near_shift = 0.4375
-ymax_near_shift = 0.5625
-zmin_near_shift = 0.4375
-zmax_near_shift = 0.5625
-
-# Mid Reagion Parameters
-xmin_mid_shift = 1.0
-xmax_mid_shift = 1.0
-ymin_mid_shift = 0.0125
-ymax_mid_shift = 0.0125
-zmin_mid_shift = 0.0125
-zmax_mid_shift = 0.0125
-
-# Far Reagion Paramters
-xmin_far_shift = 1.9375
-xmax_far_shift = 2.0625
-ymin_far_shift = 4.9375
-ymax_far_shift = 5.0625
-zmin_far_shift = 4.9375
-zmax_far_shift = 5.0625
-
-
-griddata_far = {
-    'type': 'Byrange',
-    'domain': 'full',
-    'dxyz': 0.003125,
-    'xmin': 4.9375,
-    'ymin': 4.9375,
-    'zmin': 4.9375,
-    'xmax': 5.0625,
-    'ymax': 5.0625,
-    'zmax': 5.0625,
-    'global_nx': 41,
-    'global_ny': 41,
-    'global_nz': 41,
-    'out1D_xline_y': 5.0,
-    'out1D_xline_z': 5.0,
-    'out1D_yline_x': 5.0,
-    'out1D_yline_z': 5.0,
-    'out1D_zline_x': 5.0,
-    'out1D_zline_y': 5.0,
-}
-
-griddata_mid = {
-    'type': 'Byrange',
-    'domain': 'full',
-    'dxyz': 0.003125,
-    'ymin': -0.0125,
-    'zmin': -0.0125,
-    'xmax': 2.0,
-    'ymax': 0.0125,
-    'zmax': 0.0125,
-    'global_nx': 1281,
-    'global_ny': 9,
-    'global_nz': 9,
-    'out1D_xline_y': 0.0,
-    'out1D_xline_z': 0.0,
-    'out1D_yline_x': 0.0,
-    'out1D_yline_z': 0.0,
-    'out1D_zline_x': 0.0,
-    'out1D_zline_y': 0.0,
-}
-
-griddata_near = {
-    'type': 'Byrange',
-    'domain': 'full',
-    'dxyz': 0.003125,
-    'xmin': 2.9375,
-    'ymin': 0.4375,
-    'zmin': 0.4375,
-    'xmax': 3.0625,
-    'ymax': 0.5625,
-    'zmax': 0.5625,
-    'global_nx': 41,
-    'global_ny': 41,
-    'global_nz': 41,
-    'out1D_xline_y': 0.5,
-    'out1D_xline_z': 0.5,
-    'out1D_yline_x': 3.0,
-    'out1D_yline_z': 0.5,
-    'out1D_zline_x': 3.0,
-    'out1D_zline_y': 0.5,
-}
+# Load the default values
+data = pd.read_csv('initdata.csv', sep='\t', dtype=object)
 
 # Load the data file in to dataframe
-data = pd.read_table(datafile, sep='\=', engine='python', comment='#', index_col='var')
-data = data.transpose()
+udata = pd.read_table(datafile, sep='\=', engine='python', comment='#', index_col='var')
+udata = udata.transpose()
 
 print(data)
-print(data['xmax_mid']['val'])
+
+# print(udata)
+# print(udata['xmax_mid']['val'])
