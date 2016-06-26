18272e34b8efb4ee060772d07b30cb6b7e57b5a1
Prabath Peiris <prabath.peiris@ftr.com>
2016-06-26 13:53:20 -0400

    pargenew file works

--- GenerateSimulations/pargennew.py
+++ GenerateSimulations/pargennew.py
@@ -37,20 +37,11 @@ def updatedf(data, key, col, val):
 
 # get cell value from the df
 def getVal(df, key, col='global'):
-    # try:
     val = df[df.variables == key][col].values[0]
-    # print("=========== %s %s" %(key, col))
-    # print(val)
     val = val.split(',')
     if len(val) == 1:
-        # print("Reading Key %s : Val %s" %(key, val[0]))
         return val[0]
-    # print("Reading Key %s : Val %s" %(key, val))
     return val
-    # except Exception, e:
-    #     print("=========== %s %s" %(key, col))
-    #     print(str(e))
-
 
 
 VERSION = '1.0'
@@ -66,7 +57,6 @@ parser.add_option(
 (options, args) = parser.parse_args()
 
 if (options.file is None) or (not os.path.exists(options.file)):
-    # parser.print_help()
     parser.error("Please enter a data file name with -f option")
 
 datafile = os.path.join(os.getcwd(), options.file)
@@ -89,7 +79,6 @@ for key in list(udata.columns):
 
     data = updatedf(data, nkey, col, udata[key]['val'])
 
-print(data)
 
 # Build the dir for the par file
 
@@ -100,8 +89,11 @@ if data[data.variables == 'simname'].empty:
 simname = getVal(data, 'simname')
 simdir = os.path.join(os.getcwd(), simname)
 if os.path.exists(simdir):
-    print("Simulation %s already exists, change the simname and rerun" %(simdir))
-    # sys.exit()
+    reply = str(raw_input('Simulation filed already exists at ' + simdir + ', overwrite [y/n]')).lower().strip()
+    if reply != 'y':
+        print('Abort.')
+        sys.exit()
+    print("Continue...")
 else:
     os.makedirs(simdir)
     os.makedirs(simdir + '/plots')
@@ -117,13 +109,11 @@ runScript.write("echo \"Simulation "+simname+"\" > SimulationStatus.out \n")
 runScript.write("echo \"Simulation start at \" `date -u` >> SimulationStatus.out\n")
 runScript.write("\n\n")
 runScript.write("echo \"Simulations start\"\n")
-#Plotting Script==========================================
+# Plotting Script
 plotScriptName = os.path.join(os.getcwd(), 'paroriginal/plot.py')
 sh.copy(plotScriptName, simname + '/plots/')
 newPlotScriptName = os.path.join(os.getcwd(), simname + '/plots/plot.py')
 PlotScript=open(newPlotScriptName,'a+')
-#==========================================================
-
 
 # build the par files
 cases = getVal(data, 'cases')
@@ -142,8 +132,6 @@ for case in cases:
     runScript.write("# =====================================\n")
     twopunch_resArr = getVal(data, 'twopunch_res')
 
-    print(twopunch_resArr)
-
     if not type(twopunch_resArr) is 'list':
         twopunch_resArr = [twopunch_resArr]
     exename = getVal(data, 'exename')
@@ -152,8 +140,6 @@ for case in cases:
         inputFileName = '/twopun' + case + '_' + simname + '_' + tres + '.par'
         simulationDir = '/twopun' + case + '_' + simname + '_' + tres
         outputFileName = '/twopun' + case + '_' + simname + '_' + tres + '.out'
-        print(twopParFileName)
-        print(newtwoParFileName)
         sh.copy(twopParFileName, newtwoParFileName)
         twoeditFile = open(newtwoParFileName, 'a+')
 
@@ -162,7 +148,7 @@ for case in cases:
         else:
             twopunDirlist+='\''+simulationDir+'\''
 
-        #print inputFileName
+        # print inputFileName
         runScript.write("START=$(date +%s)\n")
         runScript.write("echo \"Simulation twopun"+case+"_"+simname+"_"+tres+" start at \"`date -u` >> SimulationStatus.out\n")
         runScript.write("echo \"Still working on ...\" >> SimulationStatus.out\n")
@@ -172,7 +158,7 @@ for case in cases:
         runScript.write("echo \"Simulation took\" $(($END-$START)) \"seconds\" >> SimulationStatus.out\n")
         runScript.write("\n")
 
-        #fill the grid info
+        # fill the grid info
         twoeditFile.write('\n')
         twoeditFile.write("#--------------------------------------------------------------------------------------- \n")
         twoeditFile.write("# Grid Parameters:\n")
@@ -180,18 +166,21 @@ for case in cases:
 
         twoeditFile.write('\n')
 
-        twoeditFile.write('grid::type = \"%s\"\n' % getVal(data, 'type', case))
-        twoeditFile.write('grid::domain =\"%s\"\n' % getVal(data, 'domain', case))
-        twoeditFile.write('grid::dxyz =%s\n' % getVal(data, 'dxyz', case))
-        twoeditFile.write('grid::xmin =%s\n' % getVal(data, 'xmin', case))
-        twoeditFile.write('grid::ymin =%s\n' % getVal(data, 'ymin', case))
-        twoeditFile.write('grid::zmin =%s\n' % getVal(data, 'zmin', case))
-        twoeditFile.write('grid::xmax =%s\n' % getVal(data, 'xmax', case))
-        twoeditFile.write('grid::ymax =%s\n' % getVal(data, 'ymax', case))
-        twoeditFile.write('grid::zmax =%s\n' % getVal(data, 'zmax', case))
-        twoeditFile.write('driver::global_nx   =%s\n' % getVal(data, 'global_nx', case))
-        twoeditFile.write('driver::global_ny   =%s\n' % getVal(data, 'global_ny', case))
-        twoeditFile.write('driver::global_nz   =%s\n' % getVal(data, 'global_nz', case))
+        twoeditFile.write('grid::type       = \"%s\"\n' % getVal(data, 'type', case))
+        twoeditFile.write('grid::domain     = \"%s\"\n' % getVal(data, 'domain', case))
+        twoeditFile.write('grid::dxyz       = %s\n' % getVal(data, 'dxyz', case))
+        twoeditFile.write('grid::xmin       = %s\n' % getVal(data, 'xmin', case))
+        twoeditFile.write('grid::ymin       = %s\n' % getVal(data, 'ymin', case))
+        twoeditFile.write('grid::zmin       = %s\n' % getVal(data, 'zmin', case))
+        twoeditFile.write('grid::xmax       = %s\n' % getVal(data, 'xmax', case))
+        twoeditFile.write('grid::ymax       = %s\n' % getVal(data, 'ymax', case))
+        twoeditFile.write('grid::zmax       = %s\n' % getVal(data, 'zmax', case))
+
+        twoeditFile.write('\n')
+
+        twoeditFile.write('driver::global_nx    = %s\n' % getVal(data, 'global_nx', case))
+        twoeditFile.write('driver::global_ny    = %s\n' % getVal(data, 'global_ny', case))
+        twoeditFile.write('driver::global_nz    = %s\n' % getVal(data, 'global_nz', case))
 
         twoeditFile.write('\n')
 
@@ -254,8 +243,6 @@ for case in cases:
                 twoeditFile.write("twopunctures :: par_S_minus[2] = %s\n" % sbhval[2])
 
     # ManyBH Simulation par files
-
-
     runScript.write("# ManyBH " + case + " simulation\n")
     runScript.write("# ==================================\n")
     resArray = getVal(data, 'nzpt')
@@ -303,6 +290,9 @@ for case in cases:
         editFile.write('grid::xmax = %s\n' % getVal(data, 'xmax', case))
         editFile.write('grid::ymax = %s\n' % getVal(data, 'ymax', case))
         editFile.write('grid::zmax = %s\n' % getVal(data, 'zmax', case))
+
+        editFile.write('\n')
+
         editFile.write('driver::global_nx = %s\n' % getVal(data, 'global_nx', case))
         editFile.write('driver::global_ny = %s\n' % getVal(data, 'global_ny', case))
         editFile.write('driver::global_nz = %s\n' % getVal(data, 'global_nz', case))
@@ -341,15 +331,20 @@ for case in cases:
         editFile.write("\n")
 
         nzpt = res.split('x')
-        editFile.write("ManyBH :: nz = 10\n")
-        editFile.write("ManyBH :: np = %s\n" % nzpt[0])
-        editFile.write("ManyBH :: nt = %s\n" % nzpt[1])
-        editFile.write("ManyBH :: nr = %s\n" % nzpt[2])
-        editFile.write("ManyBH :: precis = 1.0e-9\n")
-        editFile.write("ManyBH :: ite_max = 500\n")
-        editFile.write("ManyBH :: relax = 0.5\n")
+        editFile.write("ManyBH :: nz        = 10\n")
+        editFile.write("ManyBH :: np        = %s\n" % nzpt[0])
+        editFile.write("ManyBH :: nt        = %s\n" % nzpt[1])
+        editFile.write("ManyBH :: nr        = %s\n" % nzpt[2])
+        editFile.write("ManyBH :: precis    = 1.0e-9\n")
+        editFile.write("ManyBH :: ite_max   = 500\n")
+        editFile.write("ManyBH :: relax     = 0.5\n")
+
         editFile.write("\n")
+
         editFile.write("ManyBH :: nbh = %s" % nbh)
+
+        editFile.write("\n")
+
         for i in range(0,int(nbh)):
             editFile.write("\n")
             editFile.write("ManyBH :: mbh[%s] = %s\n" % (i, mbh[i]))
