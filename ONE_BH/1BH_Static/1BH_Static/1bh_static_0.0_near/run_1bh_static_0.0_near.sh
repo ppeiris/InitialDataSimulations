34916b7141abd5a3106e9ad96713b339e5a10e86
Prabath Peiris <peiris.prabath@gmail.com>
2016-08-08 04:33:02 +0000

    static bh

---
+++ ONE_BH/1BH_Static/1BH_Static/1bh_static_0.0_near/run_1bh_static_0.0_near.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+export SIM_PATH=`pwd`
+cd $SIM_PATH
+echo "Simulation 1bh_static_0.0_near" > SimulationStatus.out 
+echo "Simulation start at " `date -u` >> SimulationStatus.out
+
+
+echo "Simulations start"
+# ManyBH near simulation
+# ==================================
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_2x3x5 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_2x3x5.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_2x3x5.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_2x3x5 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_4x5x9 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_4x5x9.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_4x5x9.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_4x5x9 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_12x13x17 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_12x13x17.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_12x13x17.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_12x13x17 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_16x17x25 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_16x17x25.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_16x17x25.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_16x17x25 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_24x25x33 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_24x25x33.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_24x25x33.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_24x25x33 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_32x33x41 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_32x33x41.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_32x33x41.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_32x33x41 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_40x41x49 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ./bbhcode $SIM_PATH/manybhnear_1bh_static_0.0_near_40x41x49.par > $SIM_PATH/manybhnear_1bh_static_0.0_near_40x41x49.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_static_0.0_near_40x41x49 end at "`date -u` >> SimulationStatus.out
+echo "Done!" >> SimulationStatus.out
