cc0e0794b1d005fc38acb998fa3bb407a217304f
Prabath Peiris <peiris.prabath@gmail.com>
2016-08-18 00:18:29 +0000

    1bh with spin

---
+++ ONE_BH/1BH_Boosted_AngularMomentum/1BH_P0.1_S0.75_convergence/1bh_p0.1_s0.75_convergence/run_1bh_p0.1_s0.75_convergence.sh
@@ -0,0 +1,273 @@
+#!/bin/sh
+export SIM_PATH=`pwd`
+cd $SIM_PATH
+echo "Simulation 1bh_p0.1_s0.75_convergence" > SimulationStatus.out 
+echo "Simulation start at " `date -u` >> SimulationStatus.out
+
+
+echo "Simulations start"
+# Twopunch near simulations
+# =====================================
+START=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_20 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_20.par > $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_20.out
+END=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_20 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_30 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_30.par > $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_30.out
+END=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_30 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_40 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_40.par > $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_40.out
+END=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_40 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_50 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_50.par > $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_50.out
+END=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_50 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_60 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_60.par > $SIM_PATH/twopunnear_1bh_p0.1_s0.75_convergence_60.out
+END=$(date +%s)
+echo "Simulation twopunnear_1bh_p0.1_s0.75_convergence_60 end at "`date -u` >> SimulationStatus.out
+
+# ManyBH near simulation
+# ==================================
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_4x5x9 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_4x5x9.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_4x5x9.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_4x5x9 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_8x9x13 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_8x9x13.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_8x9x13.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_8x9x13 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_12x13x17 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_12x13x17.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_12x13x17.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_12x13x17 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_16x17x25 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_16x17x25.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_16x17x25.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_16x17x25 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_24x25x33 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_24x25x33.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_24x25x33.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_24x25x33 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_32x33x41 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_32x33x41.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_32x33x41.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_32x33x41 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_40x41x49 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_40x41x49.par > $SIM_PATH/manybhnear_1bh_p0.1_s0.75_convergence_40x41x49.out
+END=$(date +%s)
+echo "Simulation ManyBHnear_1bh_p0.1_s0.75_convergence_40x41x49 end at "`date -u` >> SimulationStatus.out
+
+# Twopunch mid simulations
+# =====================================
+START=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_20 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_20.par > $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_20.out
+END=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_20 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_30 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_30.par > $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_30.out
+END=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_30 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_40 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_40.par > $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_40.out
+END=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_40 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_50 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_50.par > $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_50.out
+END=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_50 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_60 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_60.par > $SIM_PATH/twopunmid_1bh_p0.1_s0.75_convergence_60.out
+END=$(date +%s)
+echo "Simulation twopunmid_1bh_p0.1_s0.75_convergence_60 end at "`date -u` >> SimulationStatus.out
+
+# ManyBH mid simulation
+# ==================================
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_4x5x9 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_4x5x9.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_4x5x9.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_4x5x9 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_8x9x13 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_8x9x13.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_8x9x13.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_8x9x13 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_12x13x17 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_12x13x17.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_12x13x17.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_12x13x17 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_16x17x25 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_16x17x25.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_16x17x25.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_16x17x25 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_24x25x33 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_24x25x33.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_24x25x33.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_24x25x33 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_32x33x41 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_32x33x41.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_32x33x41.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_32x33x41 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_40x41x49 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_40x41x49.par > $SIM_PATH/manybhmid_1bh_p0.1_s0.75_convergence_40x41x49.out
+END=$(date +%s)
+echo "Simulation ManyBHmid_1bh_p0.1_s0.75_convergence_40x41x49 end at "`date -u` >> SimulationStatus.out
+
+# Twopunch far simulations
+# =====================================
+START=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_20 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_20.par > $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_20.out
+END=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_20 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_30 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_30.par > $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_30.out
+END=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_30 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_40 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_40.par > $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_40.out
+END=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_40 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_50 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_50.par > $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_50.out
+END=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_50 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_60 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_60.par > $SIM_PATH/twopunfar_1bh_p0.1_s0.75_convergence_60.out
+END=$(date +%s)
+echo "Simulation twopunfar_1bh_p0.1_s0.75_convergence_60 end at "`date -u` >> SimulationStatus.out
+
+# ManyBH far simulation
+# ==================================
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_4x5x9 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_4x5x9.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_4x5x9.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_4x5x9 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_8x9x13 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_8x9x13.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_8x9x13.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_8x9x13 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_12x13x17 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_12x13x17.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_12x13x17.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_12x13x17 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_16x17x25 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_16x17x25.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_16x17x25.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_16x17x25 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_24x25x33 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_24x25x33.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_24x25x33.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_24x25x33 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_32x33x41 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_32x33x41.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_32x33x41.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_32x33x41 end at "`date -u` >> SimulationStatus.out
+
+START=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_40x41x49 start at "`date -u` >> SimulationStatus.out
+echo "Still working on ..." >> SimulationStatus.out
+nohup ~/bbhcode $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_40x41x49.par > $SIM_PATH/manybhfar_1bh_p0.1_s0.75_convergence_40x41x49.out
+END=$(date +%s)
+echo "Simulation ManyBHfar_1bh_p0.1_s0.75_convergence_40x41x49 end at "`date -u` >> SimulationStatus.out
+
+echo "Done!" >> SimulationStatus.out
